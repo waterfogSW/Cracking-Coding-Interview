97e159d51d6cc4777fca00719d11c81f2b9294d7
San Kim <waterfog9580@gmail.com>
2021-06-28 10:49:31 +0900

    update

--- Coding Test/BruteForce/mockexam.py
+++ Coding Test/BruteForce/mockexam.py
@@ -1,3 +1,6 @@
+# 프로그래머스 : 코딩테스트 연습 > 완전탐색 > 모의고사
+# 완전 탐색 문제이나, 핵심은 finding multiple max elements index in list
+# enumerate를 통해 인덱스 출력을 간단히 할 수 있음
 def solution(answers):
     ans1 = [1,2,3,4,5]
     ans2 = [2,1,2,3,2,4,2,5]
